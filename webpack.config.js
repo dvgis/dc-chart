798e89a4bbb446fd72b48c630c25f8f0bebd6ac4
Caven <374903123@qq.com>
2020-02-03 17:30:09 +0800

    配置webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,13 @@
  * @Author: Caven
  * @Date: 2020-01-18 18:22:23
  * @Last Modified by: Caven
- * @Last Modified time: 2020-02-03 17:09:50
+ * @Last Modified time: 2020-02-03 17:28:02
  */
 
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-
-let cesiumCopyPlugin = [
-  new CopywebpackPlugin([{ from: path.join(cesiumBuild, 'Workers'), to: 'resources/Workers' }]),
-  new CopywebpackPlugin([{ from: path.join(cesiumBuild, 'Assets'), to: 'resources/Assets' }]),
-  new CopywebpackPlugin([{ from: path.join(cesiumBuild, 'Widgets'), to: 'resources/Widgets' }]),
-  new CopywebpackPlugin([{ from: path.join(cesiumBuild, 'ThirdParty'), to: 'resources/ThirdParty' }])
-]
 
 function resolve(dir) {
   return path.join(__dirname, '.', dir)
@@ -25,13 +18,9 @@ module.exports = env => {
   const IS_PROD = (env && env.production) || false
   const publicPath = IS_PROD ? '/' : '/'
   let plugins = [
-    ...cesiumCopyPlugin,
     new MiniCssExtractPlugin({
       filename: IS_PROD ? '[name].min.css' : '[name].css',
       allChunks: true
-    }),
-    new webpack.DefinePlugin({
-      CESIUM_BASE_URL: JSON.stringify('./libs/dc-sdk/resources/')
     })
   ]
   if (IS_PROD) {
@@ -49,30 +38,9 @@ module.exports = env => {
       publicPath: publicPath,
       sourcePrefix: ''
     },
-    amd: {
-      toUrlUndefinded: true
-    },
-    node: {
-      fs: 'empty'
-    },
     module: {
       unknownContextCritical: false,
       rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-          include: path.resolve(__dirname, cesiumSource),
-          use: [
-            {
-              loader: 'strip-pragma-loader',
-              options: {
-                pragmas: {
-                  debug: false
-                }
-              }
-            }
-          ]
-        },
         {
           test: /\.js$/,
           exclude: /node_modules/,
